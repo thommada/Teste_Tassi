12f2d6e1e9efa6445e8e04d7d28f780c4e2a5437
user.email <adam.thomascosta@gmail.com>
2021-06-06 12:13:39 -0300

    Modelos: Escola
    Criação da Base do Modelo da Escola

--- src/Model/Aluno.php
+++ src/Model/Aluno.php
@@ -12,7 +12,6 @@ class Aluno{
     public function save(){ // salvar um aluno no Banco de Dados
 
     }
-
     public function update(){ // atualizar um aluno no Banco de Dados
 
     }
@@ -25,41 +24,42 @@ class Aluno{
 
 
     //Getters e Setters
+    // ID
     public function getId(){ // getter ID
         return $this->id;
     }
     public function setID($id){ // setter ID
         $this->id = $id;
     }
-
+    // Nome do Aluno
     public function getNome(){ // getter nome
         return $this->nome;
     }
     public function setNome($nome){ // setter nome
         $this->nome = $nome;
     }
-
+    // Data de Nascimento
     public function getNascimento(){ // getter nascimento
         return $this->nascimento;
     }
     public function setNascimento($nascimento){ // setter nascimento
         $this->nascimento = $nascimento;
     }
-
+    // Telefone
     public function getTelefone(){ // getter telefone
         return $this->telefone;
     }
     public function setTelefone($telefone){ // setter telefone
         $this->telefone = $telefone;
     }
-
+    // E-mail
     public function getEmail(){ // getter email
         return $this->email;
     }
     public function setEmail($email){ // setter email
         $this->email = $email;
     }
-
+    // Gênero
     public function getGenero(){ // getter gênero
         return $this->genero;
     }
---
+++ src/Model/Escola.php
@@ -0,0 +1,60 @@
+<?php
+
+class Escola{
+    private $id;
+    private $nome;
+    private $endereco;
+    private $situacao;
+
+    // Funções
+    public function save(){ // salvar uma escola no Banco de Dados
+
+    }
+    public function update(){ // atualizar uma escola no Banco de Dados
+
+    }
+    public function remove(){ // remover uma escola do Banco de Dados
+
+    }
+    public function list(){ // buscar/listar escolas do Banco de Dados
+
+    }
+    public function listAluno() { // buscar/listar alunos daquela escola do Banco de Dados
+
+    }
+    public function countAlunos() { // contar alunos daquela escola do Banco de Dados
+
+    }
+
+    //Getters e Setters
+    // ID
+    public function getId(){ // getter ID
+        return $this->id;
+    }
+    public function setId($id){ // setter ID
+        $this->id = $id;
+    }
+    // Nome da Escola
+    public function getnome(){ // getter nome da escola
+        return $this->nome;
+    }
+    public function setNome($nome){ // setter nome da escola
+        $this->nome = $nome;
+    }
+    // Endereço da Escola
+    public function getEndereco(){ // getter endereço
+        return $this->endereco;
+    }
+    public function setEndereco($endereco){ // setter endereço
+        $this->endereco = $endereco;
+    }
+    // Situação
+    public function getSituacao(){ // getter Situação
+        return $this->situacao;
+    }
+    public function setSituacao($situacao){ // setter Situação
+        $this->situacao = $situacao;
+    }
+
+}
+?>
